0735c6c6210e9cf3579c560fb902f4e11b34c1ef
Mohamed Gabr <imohamedgabr@outlook.com>
2018-06-12 00:07:04 +0200

    animation is working fine

--- src/app/welcome/recent-blog-recipes/recent-blog-recipes.component.ts
+++ src/app/welcome/recent-blog-recipes/recent-blog-recipes.component.ts
@@ -2,11 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import {RecipeService } from '../../recipe.service';
 import {Observable } from 'rxjs';
 import {Recipe} from '../../recipe.interface';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+
 
 @Component({
   selector: 'app-recent-blog-recipes',
   templateUrl: './recent-blog-recipes.component.html',
-  styleUrls: ['./recent-blog-recipes.component.scss']
+  styleUrls: ['./recent-blog-recipes.component.scss'],
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 2, transform: 'translateY(-70px)' }),
+            stagger(
+              '50ms',
+              animate(
+                '550ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0px)' })
+              )
+            )
+          ],
+          { optional: true }
+        ),
+        query(':leave', animate('50ms', style({ opacity: 0 })), {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
 export class RecentBlogRecipesComponent implements OnInit {
 
